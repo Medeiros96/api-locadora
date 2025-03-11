219c6937140e0e990a07ee6b4db9cf536355ac8d
Medeiros <wellington.smedeiro1@senacsp.edu.br>
2025-03-11 19:16:16 -0300

    validaçao de email

--- src/usuario/validacao/email-unico.validator.ts
+++ src/usuario/validacao/email-unico.validator.ts
@@ -1,27 +1,35 @@
-import { ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface, registerDecorator } from "class-validator";
-import { UsuariosArmazenados } from "../usuario.dm";
-import { Injectable } from "@nestjs/common";
+import {
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  registerDecorator,
+} from 'class-validator';
+import { UsuariosArmazenados } from '../usuario.dm';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-@ValidatorConstraint({async:true})
-export class emailUnicoValidator implements ValidatorConstraintInterface{
-        constructor (private clsUsuariosArmazenados:UsuariosArmazenados){}
+@ValidatorConstraint({ async: true })
+export class emailUnicoValidator implements ValidatorConstraintInterface {
+  constructor(private clsUsuariosArmazenados: UsuariosArmazenados) {}
 
-     async validate(value: any, validationArguments?: ValidationArguments): Promise<boolean> {
-            const validarEmail = await this.clsUsuariosArmazenados.validaEmail(value);
-            return !validarEmail;
-        }
+  async validate(
+    value: any,
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> {
+    const validarEmail = await this.clsUsuariosArmazenados.validaEmail(value);
+    return !validarEmail;
+  }
 }
 
-
-    export const EmailUnico = (opcaovalidacao: ValidationOptions)=>{
-        return (objeto: object, propriedade: string) =>{
-            registerDecorator({
-                target: objeto.constructor,
-                propertyName: propriedade,
-                options: opcaovalidacao,
-                constraints: [],
-                validator: emailUnicoValidator, 
-            })
-        }
-    }
+export const EmailUnico = (opcaovalidacao: ValidationOptions) => {
+  return (objeto: object, propriedade: string) => {
+    registerDecorator({
+      target: objeto.constructor,
+      propertyName: propriedade,
+      options: opcaovalidacao,
+      constraints: [],
+      validator: emailUnicoValidator,
+    });
+  };
+};
